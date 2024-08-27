5e1bfc3dce1653855499bad854760609c24885a7
Abdullo <147158343+Abdushahidov07@users.noreply.github.com>
2024-08-27 17:11:38 +0500

    task1

---
+++ hwork.py
@@ -0,0 +1,14 @@
+# task1
+# def potatoes(a):
+#     a = a.lower() 
+#     cnt = 0
+#     b = 'potato'
+#     f = len(b)
+    
+#     for i in range(len(a) - f + 1):
+#         if a[i:i + f] == b:
+#             cnt += 1
+            
+#     return cnt
+
+# print(potatoes(input()))
